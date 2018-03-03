5920cf75dc61dba7ad9cff4a61899872795db8d8
Fercho5656 <fercho5_5_5_5@hotmail.com>
2018-03-02 23:47:13 -0600

    Add files via upload

---
+++ CameraShake.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour {
+	[Header("Values")]
+	public float duration;
+	[Range(1f,10f)]
+	public float magnitude;
+	[Range(0f,1f)]
+	public float smoothTime;
+	[Header("Booleans")]
+	[HideInInspector]
+	public bool isRunning;
+	public bool infiniteTime;
+
+	public void Update(){
+		if (Input.GetKeyDown (KeyCode.Space) && !isRunning) {
+			if (infiniteTime) {
+				StartCoroutine (Shake (float.PositiveInfinity, magnitude, smoothTime));
+			} else {
+				StartCoroutine (Shake (duration, magnitude, smoothTime));
+			}
+		}
+	}
+
+	public IEnumerator Shake(float duration, float magnitude, float smoothTime){
+		isRunning = true;
+
+		Vector3 originalPosition = transform.localPosition;
+		Quaternion originalRotation = transform.localRotation;
+
+		Vector3 velocity = Vector3.zero;
+
+		float elapsed = 0.0f;
+
+		while (elapsed < duration) {
+			float x = Random.Range (originalPosition.x - 1f, originalPosition.x + 1f) * magnitude;
+			float y = Random.Range (originalPosition.y - 1f, originalPosition.y + 1f) * magnitude;
+			//float z = Random.Range (originalRotation.z - 0.1f, originalRotation.z + 0.1f) * magnitude;
+
+			Vector3 randomPosition = new Vector3 (x, y, originalPosition.z);
+			//Quaternion randomRotation = new Quaternion (originalRotation.x, originalRotation.y, z, 1f);
+
+			gameObject.transform.localPosition = Vector3.SmoothDamp (originalPosition, randomPosition, ref velocity, smoothTime);
+			//gameObject.transform.localRotation = Quaternion.RotateTowards (originalRotation, randomRotation, 0.1f);
+
+			elapsed += Time.deltaTime;
+
+			yield return null;
+		}
+
+		isRunning = false;
+
+		gameObject.transform.localPosition = originalPosition;
+		gameObject.transform.localRotation = originalRotation;
+	}
+}
---
+++ CameraShakeEditor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CameraShake), true)]
+public class CameraShakeEditor : Editor {
+
+	public override void OnInspectorGUI (){
+
+		DrawDefaultInspector ();
+
+		CameraShake cameraShake = (CameraShake)target;
+
+		if (GUILayout.Button ("Start")) {
+			if (cameraShake.infiniteTime) {
+				cameraShake.StartCoroutine (cameraShake.Shake (float.PositiveInfinity, cameraShake.magnitude, cameraShake.smoothTime));
+			} else {
+				cameraShake.StartCoroutine (cameraShake.Shake (cameraShake.duration, cameraShake.magnitude, cameraShake.smoothTime));			
+			}
+		}
+
+		if (GUILayout.Button ("STAP! I could've dropped my croissant!")) {
+			cameraShake.StopAllCoroutines ();
+			cameraShake.isRunning = false;
+		}
+	}
+}
